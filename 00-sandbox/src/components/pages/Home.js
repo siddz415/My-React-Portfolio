7c6828c356b5902e39164a0fb8e668ce7cb11dba
Sidd <siddz415@gmail.com>
2023-05-01 17:02:56 -0700

    added linkedin, github and email icon in home.js also imported my picture and added my bio

--- 00-sandbox/src/components/pages/Home.js
+++ 00-sandbox/src/components/pages/Home.js
@@ -1,21 +1,47 @@
 import React from 'react';
 // import '../styles/Home.css';
+import linkedInIcon from "@material-ui/icons/LinkedIn";
+import emailIcon from '@material-ui/icons/Email';
+import githubIcon from "@material-ui/icons/GitHub";
+import myPic from "../assets/myPic.png";
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+    <div className="home">
+    <div className="about">
+      <h2> Hi, My Name is Siddharth Dravid</h2>
+      <img src={myPic} alt="pic" />
+
+      <div className="prompt">
+        <p></p>
+        
+        <linkedInIcon />
+        <emailIcon />
+        <githubIcon />
+      </div>
     </div>
-  );
+    <div className="skills">
+      <h1> Skills </h1>
+      <ol className="list">
+      <li className="item">
+        <h2> Front-End</h2>
+        <span>
+          HTML, CSS, Git, JavaScript, The DOM, API's, JQuery, JSON.
+        </span>
+      </li>
+      <li className="item">
+        <h2>Back-End</h2>
+        <span>
+          NodeJS, ExpressJS, MySQL, MongoDB, Sequelize.
+        </span>
+      </li>
+      <li className="item">
+        <h2>Languages</h2>
+        <span>
+          JavaScript, HTML, CSS, Ruby.
+        </span>
+      </li>
+      </ol>
+    </div>
+  </div>
+);
 }
