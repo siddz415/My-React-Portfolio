b062913e7d199feef9777471edda83d15c09f9bf
SiddD <108153549+siddz415@users.noreply.github.com>
2024-08-19 13:46:13 -0700

    updated portfolio and about me

--- 00-sandbox/src/pages/About.js
+++ 00-sandbox/src/pages/About.js
@@ -5,7 +5,7 @@ export default function Bio() {
     <div>
       <h1>About Me</h1>
       <p>
-        As a Full Stack Developer, I have experience working with various programming languages including JavaScript, HTML, CSS, and Python. I have also worked with multiple front-end frameworks such as React, Angular, and Vue.js, as well as back-end frameworks like Node.js, Flask and Django. This diverse skillset has allowed me to develop robust and scalable web applications that meet clients needs across various industries.
+        As a Full Stack Developer, I have experience working with various programming languages including JavaScript, HTML, CSS, and Python. I have also worked with multiple front-end frameworks such as React, Angular, and Vue.js, as well as back-end frameworks like Node.js, Flask and Django. This diverse skillset has allowed me to develop robust and scalable web applications that meet clients needs across various industries. Familiar with cloud services and containerization using AWS, and Docker, and experienced in using Git and Agile methodologies. Known for excellent problem-solving and debugging skills, with a strong focus on delivering high-quality, user-friendly products. I am passionate about learning new technologies and collaborating with other developers to create innovative solutions.  
       </p>
     </div>
   );
--- 00-sandbox/src/pages/Portfolio.js
+++ 00-sandbox/src/pages/Portfolio.js
@@ -6,6 +6,9 @@ import Porfolio1 from '../assets/portfolio.png';
 import Quiz from '../assets/quiz.png';
 import Spiral from '../assets/spiral.jpg';
 import Workday from '../assets/workday.png';
+import SQL from '../assets/sql.png';
+import Rag from '../assets/rag.png';
+import Gamesrus from '../assets/gamesrus.png';
 //using portfolioitems to render the name and url of the projects
 import '../styles/Portfolio.css';
 export default function myPortfolio() {
@@ -13,6 +16,9 @@ export default function myPortfolio() {
     <div className='portfolio'>
       <h1>My Portfolio</h1>
       <div className='portfolioList'>
+      <PortfolioItems name="Chat-with-MySql" image={SQL} url="https://github.com/siddz415/Chat-with-MySql-Openai" /> 
+      <PortfolioItems name="Langchain-RAG" image={Rag} url="https://github.com/siddz415/Langchain-RAG" /> 
+      <PortfolioItems name="GamesRUS" image={Gamesrus} url="https://github.com/siddz415/GamesRUS" /> 
         <PortfolioItems name="Turn Up" image={Turnup} url="https://github.com/siddz415/Turn-Up" /> 
         <PortfolioItems name="Password Generator" image={PasswordG} url="https://github.com/siddz415/Password-generator" />
         <PortfolioItems name="HTML/CSS Portfolio" image={Porfolio1} url="https://github.com/siddz415/My-personal-portfolio" />
